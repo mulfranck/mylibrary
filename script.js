7a94276e24ccb6993575754a5f270e94bf4edbfb
mulfranck <mulfranck5@gmail.com>
2022-01-09 15:59:21 +0100

    The haveRead status changes on link but the html element is not updating.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-
 let $container = document.querySelector('.container');
-
 
 let myLibrary = [
     {
@@ -10,6 +8,14 @@ let myLibrary = [
         pages : 298,
         haveRead: true,
         id: '3434',
+        info: function() {
+            return `${this.haveRead}`
+        },
+        toggler: function(){
+            console.log(this.haveRead);
+            this.haveRead = !this.haveRead;
+            console.log(this.haveRead)
+        }
     },
     {
         title: 'Mindset',
@@ -30,6 +36,10 @@ function BookCreator(title, author, pages, read) {
 BookCreator.prototype.info = function() {
     return (` The ${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead}.`);
 }
+BookCreator.prototype.readStatusToggler = function() {
+    this.haveRead = !this.haveRead;
+    console.log(this.haveRead)
+}
 
 
 
@@ -41,25 +51,37 @@ function addBookToLibrary() {
 }
 
 function displayAllBooks() {
+    let counter = 0;
     for (books of myLibrary) {
         let status = books.haveRead ? 'Have read' : 'Have not read';
 
         $container.innerHTML += 
         `
-        <article class='card'>
+        <article class='card' data-id='${counter}'>
             <header class='card-head'>
                 <h2 class='title'> ${books.title} </h2>
             </header>
             <section class='card-body'>
                 <div> 
                     <p> Wrote by <span author> ${books.author} <span></p>
-                    <p> <span class='pages'>${books.pages}</span> pages, <span class='status'>${status}</span>.</p>
+                    <p> <span class='pages'>${books.pages}</span> pages, <span class='status'>${books.haveRead}</span>.</p>
                 </div>
+            </section>
+            <section class='card-footer'>
+                <button id='readStatus' onclick="{myLibrary[${counter}].toggler()}">${books.haveRead}</button>
+                <button id='editor'>Edit</button>
             </section>
         </article>
         `;
+        ++counter;
 
     }
 }
 
 displayAllBooks();
+// function toggleRead(index) {
+//     myLibrary[index].toggler();
+    
+
+//     console.log(myLibrary[index].info())
+// }
