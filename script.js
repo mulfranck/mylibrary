f0d55c8fb1c433c2db16ec35f6a98cb481eb0264
mulfranck <mulfranck5@gmail.com>
2022-01-24 23:16:37 +0100

    The submit button function added
    The submit button added the inputed info into the array of books as an object Book
    
    A bookkey or more like the book index, to aid in adding the newly added book into the array

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-let $container = document.querySelector('.container');
-let $formContainer = document.querySelector('.form-container')
+let $container = document.querySelector('.container'); //the main container whr all the books are rendered.
+let $formContainer = document.querySelector('.form-container');
+let $form = document.getElementById('addBookForm');//actual form element
 
 let $addbtn = document.querySelector('#addbookbtn');
+
 
 let myLibrary = [
     {
@@ -10,79 +12,83 @@ let myLibrary = [
         author: 'Cal Newport',
         pages : 298,
         haveRead: true,
-        id: '3434',
-        info: function() {
-            return `${this.haveRead}`
-        },
-        toggler: function(){
-            console.log(this.haveRead);
-            this.haveRead = !this.haveRead;
-            console.log(this.haveRead)
-        }
     },
     {
         title: 'Mindset',
         author: 'NaN',
         pages: 359,
         haveRead: false,
-        id: 7890,
     }
 ];
+let bookKey = myLibrary.length-1;
 
-function BookCreator(title, author, pages, read) {
+function BookCreator(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
 }
 
-BookCreator.prototype.info = function() {
-    return (` The ${this.title} by ${this.author}, ${this.pages} pages, ${this.haveRead}.`);
-}
-BookCreator.prototype.readStatusToggler = function() {
-    this.haveRead = !this.haveRead;
-    console.log(this.haveRead)
-}
 
-
-
-function addBookToLibrary() {
+const addBookForm = () => {
     $formContainer.classList.toggle('hide');
     $container.classList.toggle('hide');
-
-    // let book = new BookCreator(title, author, pages, read);
-
-    // myLibrary.push(book);
 }
 
-function displayAllBooks() {
-    let counter = 0;
-    for (books of myLibrary) {
-        let status = books.haveRead ? 'Have read' : 'Have not read';
+function addBookToLibrary(title, author, pages, read) {
 
-        $container.innerHTML += 
+    myLibrary.push(new BookCreator(title, author, pages, read));
+}
+
+
+
+const display_a_book = index => {
+    $container.innerHTML += 
         `
-        <article class='card' data-id='${counter}'>
+        <article class='card' data-id='${index}'>
             <header class='card-head'>
-                <h2 class='title'> ${books.title} </h2>
+                <h2 class='title'>loop number ${index} ${myLibrary[index].title} </h2>
             </header>
             <section class='card-body'>
                 <div> 
-                    <p> Wrote by <span author> ${books.author} <span></p>
-                    <p> <span class='pages'>${books.pages}</span> pages, <span class='status'>${books.haveRead}</span>.</p>
+                    <p> Wrote by <span author> ${myLibrary[index].author} <span></p>
+                    <p> <span class='pages'>${myLibrary[index].pages}</span> pages, 
+                        <span class='status'>${myLibrary[index].haveRead}</span>.
+                    </p>
                 </div>
             </section>
             <section class='card-footer'>
-                <button id='readStatus' onclick="{myLibrary[${counter}].toggler()}">${books.haveRead}</button>
+                <button id='readStatus' onclick="{myLibrary[${index}].toggler()}">
+                    ${myLibrary[index].haveRead}
+                </button>
                 <button id='editor'>Edit</button>
             </section>
         </article>
         `;
-        ++counter;
-
-    }
 }
 
-displayAllBooks();
+(function displayAllBooks() {
+    let counter = 0;
+    for (let i = 0; i <= bookKey; i++){
+        display_a_book(i)
+    }
 
-$addbtn.addEventListener('click', addBookToLibrary)
+})();
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    let form_data = document.getElementById('addBookForm').elements;
+    
+    addBookToLibrary(form_data['title'].value, form_data['author'].value, 
+            form_data['pages'].value, form_data['haveRead'].checked)
+    
+    bookKey++; //increment the book key as the book is added.
+    display_a_book(bookKey)//display the currently book
+    addBookForm();//to toggle the form container and the main container
+    form_data['title'].value = form_data['author'].value = 
+        form_data['pages'].value = ""; //reset the input control to empty
+    
+}
+$form.addEventListener('submit', handleSubmit)
+$addbtn.addEventListener('click', addBookForm)
